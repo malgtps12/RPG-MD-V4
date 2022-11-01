02fcc11b37877c51151ee531b393cbbd65410dad
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-11-02 00:20:49 +0700

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -30,9 +30,9 @@ ${botdate}
                         mediaType: 2,
                         description: 'anu',
                         title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
-                        body: `Subscribe YT My Bestie`,                                       previewType: 0,
+                        body: `-`,                                       previewType: 0,
                         thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
-                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+                        sourceUrl: '-'
             }
         }
     })
