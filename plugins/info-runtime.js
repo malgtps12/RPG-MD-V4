a2e3d82079417224c8a4b60460bf0990eed3e9c5
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-11-02 00:15:30 +0700

    Update info-runtime.js

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -35,9 +35,9 @@ const time = moment.tz('Asia/Jakarta').format('HH')
                         mediaType: 2,
                         description: 'anu',
                         title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
-                        body: `Subscribe YT My Bestie`,                                       previewType: 0,
+                        body: `-`,                                       previewType: 0,
                         thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
-                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+                        sourceUrl: '-'
             }
         }
     })
